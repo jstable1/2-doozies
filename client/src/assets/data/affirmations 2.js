09800650163741788fb01a8d2de02568b425e852
jstable1 <jessica.stabler@gmail.com>
2022-05-24 21:14:00 -0600

    deploy to heroku

---
+++ client/src/assets/data/affirmations 2.js
@@ -0,0 +1,37 @@
+const affirmationList = [
+  {
+    affirmation: "Be yourself, there is no one better",
+  },
+  {
+    affirmation: "The best investment I made was investing in myself",
+  },
+  {
+    affirmation: "Stepping out of my comfort zone is necessary for growth",
+  },
+  {
+    affirmation: "I am and always will be good enough, smart enough and strong enough",
+  },
+  {
+    affirmation: "I will only welcome happiness into my circle",
+  },
+  {
+    affirmation: "I can become totally new overnight, no matter how long i've been living a certain way",
+  },
+  {
+    affirmation: "There are people whose lives are better because of me",
+  },
+  {
+    affirmation: "I choose confidence and calm over worry and stress",
+  },
+  {
+    affirmation: "I release the past and move into the future with joy and excitement",
+  },
+  {
+    affirmation: "Loving myself isn't selfish",
+  },
+  {
+    affirmation: "I am proud of the hard work I put into things",
+  },
+];
+
+export default affirmationList;
