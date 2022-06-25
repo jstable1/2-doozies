65c520a9cffbb5248e59276248ae41a9f33d7e9d
jstable1 <jessica.stabler@gmail.com>
2022-06-25 16:21:06 -0600

    removed calendar package

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { CalendarComponent } from "@syncfusion/ej2-react-calendars";
 
 const Calendar = () => {
   return (
     <div className="calendar">
-      <CalendarComponent></CalendarComponent>
     </div>
   );
 };
